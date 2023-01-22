03f9957e5f45a7c79a5f5f6aae07c5e6cbd193b0
arsenblade <arsenblade@mail.ru>
2023-01-22 20:06:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainPage from './pages';
+import MyToastContainer from './ui/MyToast/MyToastContainer';
+
+function App() {
+  return (
+    <div className="app">
+      <MainPage />
+      <MyToastContainer />
+    </div>
+  );
+}
+
+export default App;
